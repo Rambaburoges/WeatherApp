51c67d0826a2be0b8eb26f162281abc60c2863ad
rambabu <rambabu.roges96@gmail.com>
2020-09-13 23:59:38 +0530

    Updates

---
+++ precache-manifest.6d2567d4bb141062bdab18c6c5384582.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c741ae6cbbc59b05966e44494cf695",
+    "url": "/Rambaburoges/WeatherApp/index.html"
+  },
+  {
+    "revision": "3fe798f875580a9d8cf1",
+    "url": "/Rambaburoges/WeatherApp/static/css/main.8daa3b20.chunk.css"
+  },
+  {
+    "revision": "5fae6b1de25c9e99106f",
+    "url": "/Rambaburoges/WeatherApp/static/js/2.0258b798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rambaburoges/WeatherApp/static/js/2.0258b798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe798f875580a9d8cf1",
+    "url": "/Rambaburoges/WeatherApp/static/js/main.2f74bb68.chunk.js"
+  },
+  {
+    "revision": "c2aa7709e3a0ae4d7192",
+    "url": "/Rambaburoges/WeatherApp/static/js/runtime-main.e27430c9.js"
+  },
+  {
+    "revision": "b7fd60ea2f92f10904afba5228b15c46",
+    "url": "/Rambaburoges/WeatherApp/static/media/cloud-bg.b7fd60ea.jpg"
+  },
+  {
+    "revision": "288d5e7587fd3934db6416269dcddbd5",
+    "url": "/Rambaburoges/WeatherApp/static/media/warm-bg.288d5e75.jpg"
+  }
+]);
